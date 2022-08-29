3fc9603d74caeab66336f062d193aa1c838019b1
Justin Cooper <jcooper@chicousd.org>
2022-08-29 13:08:47 -0700

    install module

--- Restart-VCenterHosts.ps1
+++ Restart-VCenterHosts.ps1
@@ -290,6 +290,7 @@ filter Skip-RecentlyRebootedHosts {
 . .\lib\Show-TestRun.ps1
 # ===============================================
 Show-TestRun
+Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force -Confirm:$false
 # $env:psmodulepath = 'C:\Program Files\WindowsPowerShell\Modules; C:\Windows\system32\config\systemprofile\Documents\WindowsPowerShell\Modules; C:\Program Files (x86)\WindowsPowerShell\Modules; C:\Windows\system32\WindowsPowerShell\v1.0\Modules; C:\Program Files (x86)\VMware\Infrastructure\PowerCLI\Modules'
 Import-VMwareModules
 
